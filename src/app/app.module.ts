b073400bc5139240c9dab42c0adbef3b97806be3
unknown <caisyah0808@gmail.com>
2022-01-27 20:59:13 +0700

    menambahkan server component di app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,29 @@
+/*  Q: Apa itu module ? 
+    A:  module adalah wadah (container), dengan kata lain module bisa didefinisikan sebagai : 
+        1. mekanisme untuk mengelompokkan *components*, *services*, *directives*, dan *pipes* 
+        // source : https://dotnettutorials.net/lesson/modules-angular-application/
+        2. gabungan fungsionalitas yang memberitahu angular fitur2 apa sih yang digunakan oleh aplikasi kita.
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+// 4. itulah mengapa ServerComponent juga perlu di import di atas.
+// 5. btw, nama ServerComponent itu berasal dari class yg ada di server.component.ts lho
+import { ServerComponent } from './server/server.component';
 
+// @ adalah penanda bahwa dia adalah decorator jadi NgModule adalah decorator
 @NgModule({
+/*  1.  brhubung angular TIDAK SCAN semua file yg kita punya, kalau kita dideklarasi componen kita maka angular tidak akan tahu component
+        baru yg kita buat.
+    2.  Itulah kenapa ServerComponent yg kita baru buat, harus dideklarasi di declarations.
+*/
   declarations: [
-    AppComponent
+    AppComponent,
+  /*  3.  berhubung angular ini menggunakan typescript, jadi kalau hanya deklarasi di "declarations" saja akan membuat error
+          karena typescript tidak tahu dimana bisa menemukan serverComponent ini 
+  */
+    ServerComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,27 @@
+// 3. since typescript wouldn't know component before you import it, so we need to import it first like below
+import { Component } from "@angular/core";
+
+/*  1. we need component to use other directives whether it is custom or builtin in the @Component annotation ( source : https://stackoverflow.com/questions/34613065/what-is-the-difference-between-component-and-directive) 
+    2. that's why we write it down below ( source : https://angular.io/guide/component-overview)
+    4. just empty component wouldn't do, it's nothing valuable for angular so we need to add 
+        4.1 selector
+        // source : https://www.pluralsight.com/guides/understanding-the-purpose-and-use-of-the-selector-in-angular
+            4.1.1 Selector berfungsi untuk mengidentifikasi setiap component secara unik di component tree 
+            4.1.2 representasi dari component ini di HTML DOM
+        4.2 templateUrl
+        // source : https://angular.io/guide/component-overview
+            4.2.1 lokasi(path) template html yang akan dipakai di component typescript ini    
+        4.3 styleUrls ( optional ) 
+        // source : https://angular.io/guide/component-overview
+            4.3.1 lokasi(path) file css yang akan dipakai di component typescript ini. bentuknya array krn bisa nerima lebh dari 
+                  satu file css
+*/
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent{
+    // class disini adalah tempat kamu menaruh logika-mu 
+    // di sini class ServerComponent akan menciptakan objek berdasarkan logika-mu dan di ekspor supaya bisa dipakai di tempat lain.
+}
